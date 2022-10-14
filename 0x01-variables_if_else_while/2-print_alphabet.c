bf58b20dc259702a8331d4cc4fc6ed91daa261fa
mohammed ismail <warethco@gmail.com>
2022-10-14 12:18:01 -0700

    create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the English alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+		putchar(letter);
+		putchar('\n');
+		letter++;
+	}
+
+	return (0);
+}
