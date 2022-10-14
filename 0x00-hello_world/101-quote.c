6c1675b046b8bca1a1a475896dd3c8eb23f17d86
mohammed ismail <warethco@gmail.com>
2022-10-14 00:33:36 -0700

    create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "and that piece
+ * of art is useful" - Dora Korpar, 2015-10-19"
+ * to the standard error
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art");
+	fprintf(stderr, "is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
