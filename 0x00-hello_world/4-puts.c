ca39eeddf9790d1fc80a8d075206824deb78627e
mohammed ismail <warethco@gmail.com>
2022-10-13 23:29:47 -0700

    some fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ *This program outputs Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
