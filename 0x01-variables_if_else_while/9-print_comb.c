4028f2ebd5e6b21db32696cd6f19b7f0188249db
mohammed ismail <warethco@gmail.com>
2022-10-14 13:27:08 -0700

    create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers from 0 to 9
+ * separated by comma (0, 1, 2, 3, 4, 5, 6, 7, 8, 9$)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+
+		if (number == 57)
+			putchar(36);
+		else
+		{
+			putchar(44);
+			putchar(32);
+		}
+		number++;
+	}
+
+	return (0);
+}
