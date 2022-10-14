45f9c890a326481cca58eb3f6a4a5a3c4115067d
mohammed ismail <warethco@gmail.com>
2022-10-14 00:37:28 -0700

    some fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art");
+	fprintf(stderr, "and that piece of art ");
 	fprintf(stderr, "is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
