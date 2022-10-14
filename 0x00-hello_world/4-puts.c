c92bf392f9dd26feccc47a0a4f53f59209364f6d
mohammed ismail <warethco@gmail.com>
2022-10-13 23:37:50 -0700

    some fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-/*
- *This program outputs Programming is like building a multilingual puzzle
+/**
+ * main - Entry point
+ *
+ * Description: 'prints the string "Programming is like building a multilingual
+ *puzzle" followed by a new line'
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
