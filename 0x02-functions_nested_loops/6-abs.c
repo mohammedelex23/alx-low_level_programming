0532b7ed5f050ad0065bc3451ea95f9ea2664abb
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2022-10-18 23:29:54 +0200

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - the function
+ * @n: a number
+ * Description: converts negative to
+ * positive
+
+* Return: positive number
+ */
+int _abs(int n)
+{
+	return ((n < 0) ? -1 * n : n);
+}
