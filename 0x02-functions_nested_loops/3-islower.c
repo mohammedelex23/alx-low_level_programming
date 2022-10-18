599034802a60ef9062fa5e8e234932cd93d53f7a
mohammed ismail <warethco@gmail.com>
2022-10-18 06:57:08 -0700

    create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+/**
+ * _islower - the function
+ * Description: return 1 for lower letter
+ * and 0 for not
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ void printSmth(void)
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
