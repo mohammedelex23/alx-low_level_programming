670b1daaafa3d2422df31a9950f7430ee0f83f2c
mohammed ismail <warethco@gmail.com>
2022-10-18 04:05:02 -0700

    create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
