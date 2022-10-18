81885924d859daea68bdf3efea9e49905b4ea087
mohammed ismail <warethco@gmail.com>
2022-10-18 05:54:12 -0700

    some fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,18 +9,4 @@ void printSmth(void)
 	puts("_putchar");
 }
 
-/**
- * print_alphabet - the function
- * Description: prints English alphabet
- */
-void print_alphabet(void)
-{
-	int letter = 97;
-
-	while (letter <= 122)
-	{
-		putchar(letter);
-		letter++;
-	}
-	putchar('\n');
-}
+void print_alphabet(void);
