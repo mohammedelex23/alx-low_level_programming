92d9095f3e0212dfe64d87c51f28c0638ea34394
mohammed ismail <warethco@gmail.com>
2022-10-18 05:00:40 -0700

    create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Calls print_alphabet that
+ * prints English alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	print_alphabet();
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,19 @@ void printSmth(void)
 {
 	puts("_putchar");
 }
+
+/**
+ * print_alphabet - the function
+ * Description: prints English alphabet
+ */
+void print_alphabet(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
