cbd0846a89724b548706b040fb8d01101ac48099
mohammed ismail <warethco@gmail.com>
2022-10-18 06:19:44 -0700

    create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+/**
+ * print_alphabet_x10 - the function
+ * Description: prints English alphabet
+ * 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 10;
+
+	while (i > 0)
+	{
+		int letter = 97;
+		while (letter <= 122)
+		{
+			putchar(letter);
+			letter++;
+		}
+		putchar('\n');
+		i--;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void printSmth(void)
 }
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
