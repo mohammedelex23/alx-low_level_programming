fb12e4b72f6a6b9c8e2246b9c1e29c46e0f21dcb
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2022-10-19 17:02:24 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
+
 /**
- * times_table - the function
- * Description: prints timetable
- * Return: does not return anything
+ *times_table - the function
+ *Description: prints timetable
+ *Return: does not return anything
  */
 void times_table(void)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; i <= 9; i++)
-        {
-            for (j = 0; j <= 9; j++)
-            {
-                    if (j == 9)
-                            printf("%d", j * i);
-                    else
-                            if ((i) * (j + 1) < 10)
-                                    printf("%d,  ", j * i);
-                            else
-                                    printf("%d, ", j * i);
-            }
-            printf("\n");
-        }        
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 9)
+				printf("%d", j *i);
+			else
+			if ((i) *(j + 1) < 10)
+				printf("%d,  ", j *i);
+			else
+				printf("%d, ", j *i);
+		}
+
+		printf("\n");
+	}
 }
