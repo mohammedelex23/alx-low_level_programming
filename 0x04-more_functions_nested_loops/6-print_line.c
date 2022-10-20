a2d48cb716aed5c69ec78c5144d626804837d71f
mohammed ismail <warethco@gmail.com>
2022-10-20 03:59:13 -0700

    create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - the function
+ * @n: a number
+ * Description: prints a line of
+ * length n
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	if (n < 1)
+	{
+		goto end;
+		return;
+	}
+
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar(95);
+		i++;
+	}
+
+end:
+	_putchar('\n');
+
+}
