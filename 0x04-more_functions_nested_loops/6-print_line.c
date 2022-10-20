9a95e7ef2d77e250a72b6999810fef6ac7e73cdb
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2022-10-20 13:06:09 +0200

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,18 @@
-#include "main.h"
-
-/**
- * print_line - the function
- * @n: a number
- * Description: prints a line of
- * length n
- * Return: nothing
- */
-
 void print_line(int n)
 {
-	if (n < 1)
-	{
-		_putchar('\n);
-		return;
-	}
+        if (n < 1)
+        {
+                _putchar('\n');
+        }
+        else
+        {
+                int i = 0;
 
-	int i = 0;
-
-	while (i < n)
-	{
-		_putchar(95);
-		i++;
-	}
-	_putchar('\n');
+                while (i < n)
+                {
+                        _putchar(95);
+                        i++;
+                }
+                _putchar('\n');
+        }
 }
