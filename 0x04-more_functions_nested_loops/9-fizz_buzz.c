6ee79ea09ce8236c607ea4c1c2dba4c838e5e6c7
mohammed ismail <warethco@gmail.com>
2022-10-20 05:00:03 -0700

    create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - the function
+ * Description: a fizzbuzz program
+ * Return: 0 (always 0)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i < 101)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
