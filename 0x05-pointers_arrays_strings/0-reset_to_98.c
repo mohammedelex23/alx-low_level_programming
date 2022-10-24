1a8b30e909555271e9971d46779a1c61214c19d5
mohammed ismail <warethco@gmail.com>
2022-10-24 12:12:21 -0700

    create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the function
+ * @n: a pointer
+ * Description: changes the value of the address
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
