d32ea07ceb01caf12f487bbdc494391610eff167
mohammed ismail <warethco@gmail.com>
2022-10-26 12:47:01 -0700

    some fix

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - the function
+ * @dest: first pointer
+ * @src: second pointer
+ * @n: a number
+ * Description: copy string
+ * Return: concatinated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i = 0;
+
+	while (i < n && dest[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+
+}
