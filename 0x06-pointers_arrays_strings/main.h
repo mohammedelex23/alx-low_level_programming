758ac5288c4871d9db34653a7363712ee5a5f544
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2022-10-27 11:42:14 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,12 +3,12 @@
 
 #include <stdio.h>
 
-char *cap_string(char *str);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
+char *cap_string(char *str);
 
 #endif /* MAIN_H */
