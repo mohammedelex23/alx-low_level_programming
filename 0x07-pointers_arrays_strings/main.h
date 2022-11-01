e64d1ef4a3088d6126f4f0def0268bb950958b4b
mohammed ismail <warethco@gmail.com>
2022-11-01 12:35:19 -0700

    some fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
 
 #endif /* MAIN_H */
