bb0faf76b7d420acf9d6c9fd9400886cd29fda17
mohammed ismail <warethco@gmail.com>
2022-11-04 07:36:29 -0700

    some fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the function
+ * Description: prints this program name
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
