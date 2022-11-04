f7db421a5950c208207e4ce811a0859a94de194a
mohammed ismail <warethco@gmail.com>
2022-11-04 08:33:43 -0700

    some fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!(atoi(argv[i]) >= 0 && atoi(argv[i]) <= 9))
+		if (!((0 + argv[i][0]) >= 48 && 0 + (argv[i][0] <= 57)))
 		{
 			puts("Error");
 			return (1);
