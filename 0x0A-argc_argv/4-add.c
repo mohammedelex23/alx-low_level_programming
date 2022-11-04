002fd632769fae33ceeca05b8e5a17e89c3c22b1
mohammed ismail <warethco@gmail.com>
2022-11-04 08:28:04 -0700

    some fix

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - the function
+ * @argc: arguments count
+ * @argv: char array of arguments
+ * Description: prints the arguments count
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!(atoi(argv[i]) >= 0 && atoi(argv[i]) <= 9))
+		{
+			puts("Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
