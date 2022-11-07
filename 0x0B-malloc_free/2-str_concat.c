14d1bae799cfbe85fc0b390b4190721424041f38
mohammed ismail <warethco@gmail.com>
2022-11-07 03:05:27 -0800

    some fix

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates string length
+ * @str: a string pointer
+ * Return: the length of the string: @str
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * str_concat - Concatinates @s1 and @s2
+ * @s1: first string
+ * @s2: second string
+ * Return: an address of newly concatinated strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int s1_size;
+	int s2_size;
+	char *result;
+	int i = 0;
+	int j = 0;
+
+	if (s1 == NULL)
+		s1_size = 0;
+	else
+		s1_size = _strlen(s1);
+	if (s2 == NULL)
+		s2_size = 0;
+	else
+		s2_size = _strlen(s2);
+
+	result = malloc(sizeof(char) * (s1_size + s2_size + 1));
+
+	while ((s1 != NULL && s1[i] != '\0') || (s2 != NULL && s2[j] != '\0'))
+	{
+		if (s1 != NULL && s1[i] != '\0')
+		{
+			result[i] = s1[i];
+			i++;
+		}
+		else
+		{
+			result[i + j] = s2[j];
+			j++;
+		}
+	}
+
+	result[i + j] = '\0';
+	return (result);
+}
