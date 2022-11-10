ada9e418fb8bd124ee9b05fcee18552411e975da
mohammed ismail <warethco@gmail.com>
2022-11-10 04:17:25 -0800

    some fix

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * malloc_checked - allocate a space in heap memory
+ * @b: an int
+ * Return: a pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if ((ptr == NULL)
+	    exit(98);
+	return (ptr);
+}
