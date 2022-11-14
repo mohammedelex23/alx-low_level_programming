d5c5f610d49fad7a3feb7d0182880c28304c34e8
mohammed ismail <warethco@gmail.com>
2022-11-14 03:56:27 -0800

    some fix

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	const char *name;
+	float age;
+	const char *owner;
+}
+
+	#endif
