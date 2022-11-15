1b27f6f6ea26301ef6d744405e9428aa20cfe209
mohammed ismail <warethco@gmail.com>
2022-11-14 19:50:24 -0800

    some fix

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#ifndef MAIN
+#define MAIN
+    #include <stdio.h>
+
+    #ifndef PRINT_FILE
+#define PRINT_FILE() puts(__FILE__)
+    #endif
+#endif
+
+
+/**
+ * main - prints the file name of this
+ * program
+ * Return: always 0
+ */
+int main(void)
+{
+	PRINT_FILE();
+	return (0);
+}
