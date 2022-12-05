b3a07ccaf014aba805dc24377e24f24ea5715a9d
mohammed ismail <warethco@gmail.com>
2022-12-05 05:02:13 -0800

    some fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,50 @@
 #include "main.h"
 
 /**
- * read_textfile - Entry Point
- * @filename: file name
- * @letters: size
- * Return: 0
+ * read_textfile - read text file and print it to POSIX stdout
+ * @filename: filename to open and read
+ * @letters: number of letter to read and print
+ *
+ * Return: 0 if failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
+	ssize_t fd, numread, numwrote;
 	char *buf;
+
 
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
+	/* OPEN */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
 		return (0);
+	}
 
-	buf = malloc(sizeof(char) * letters + 1);
+	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
+	{
+		close(fd);
 		return (0);
+	}
 
-	rd = read(file, buf, letters);
-	if (rd == -1)
+	/* READ */
+	numread = read(fd, buf, letters);
+	close(fd);
+	if (numread == -1)
+	{
+		free(buf);
 		return (0);
+	}
 
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-	if (wr == -1)
-		return (0);
-
-	close(file);
+	/* WRITE */
+	numwrote = write(STDOUT_FILENO, buf, numread);
 	free(buf);
-	return (wr);
+
+	if (numread != numwrote)
+		return (0);
+
+	return (numwrote);
 }
