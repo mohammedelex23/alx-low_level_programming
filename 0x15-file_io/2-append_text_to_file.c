f6910f9effef3599f61c2c67a55fbb9462390036
mohammed ismail <warethco@gmail.com>
2022-12-05 04:58:47 -0800

    some fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,43 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Entry Point
- * @filename: file name
- * @text_content: text content
- * Return: 1
+ * append_text_to_file - function that appends text to the end of the file
+ * @filename: filename to append text to
+ * @text_content: NULL terminated string
+ *
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, i = 0;
+	int fd, numwrote, length;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_APPEND);
-	if (file == -1)
-		return (-1);
-
-
-
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	/* return -1 if file doesn't exist or no privilege to write */
+	if (fd == -1)
 	{
-		close(file);
+		close(fd);
+		return (-1);
+	}
+
+	/* if NULL string and file exists return 1 */
+	if ((text_content == NULL) && (fd != -1))
+	{
+		close(fd);
 		return (1);
 	}
-	else
-	{
-		wr = write(file, text_content, i);
-	}
 
-	if (wr == -1)
+	length = 0;
+	while (text_content[length] != '\0')
+		length++;
+
+	numwrote = write(fd, text_content, length);
+
+	/* if write failed or didn't write full string */
+	if (numwrote == -1 || numwrote != length)
 		return (-1);
 
-	close(file);
 	return (1);
 }
