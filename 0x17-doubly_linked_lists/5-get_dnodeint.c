c88441a4c4facf788a1c016b5b59220505bb265b
mohammed ismail <warethco@gmail.com>
2023-01-12 08:39:29 -0800

    some fix

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - find node at index
+ * @head: list head
+ * @index: target index
+ * Return: the found node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index - 1)
+			return (head);
+		i++;
+		head = head->next;
+	}
+
+	return (NULL);
+}
