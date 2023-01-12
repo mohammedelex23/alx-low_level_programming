edced9d10d361010da502abbce96cd4b1fb2ec92
mohammed ismail <warethco@gmail.com>
2023-01-12 12:13:22 -0800

    some fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (idx == 0)
 			{
 				temp->prev = new_node;
-				*head = new_node;
+				*h = new_node;
 				return (new_node);
 			}
 			else
