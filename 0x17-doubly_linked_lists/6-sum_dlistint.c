88c77a6ebb9519502437810559597a69aea9658b
mohammed ismail <warethco@gmail.com>
2023-01-12 08:11:25 -0800

    some fix

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum all nodes
+ * @head: list head
+ * Return: sum of nodes
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
